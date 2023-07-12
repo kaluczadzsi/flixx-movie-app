6bbaa84baac5e04b83c940a890d3736e952276ad
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-07-12 19:18:51 +0200

    Alert and searchAPIData function added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-const global = { currentPage: window.location.pathname };
+const global = {
+  currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    key: '43a6893854179bc8709d5b02acc60c8f',
+    url: 'https://api.themoviedb.org/3/',
+  },
+};
 
 // Display 20 most popular movies
 async function displayPopularMovies() {
@@ -226,6 +238,22 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    const results = await searchAPIData();
+    console.log(results);
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIDAta('movie/now_playing');
@@ -273,13 +301,30 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIDAta(endpoint) {
-  const API_KEY = '43a6893854179bc8709d5b02acc60c8f';
-  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = global.api.key;
+  const API_URL = global.api.url;
 
   showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+async function searchAPIData() {
+  const API_KEY = global.api.key;
+  const API_URL = global.api.url;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
   );
 
   const data = await response.json();
@@ -308,6 +353,19 @@ function highlightActiveLink() {
   });
 }
 
+// Show Alert
+function showAlert(message, className) {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => {
+    alertEl.remove();
+  }, 3000);
+}
+
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
@@ -330,7 +388,7 @@ function init() {
       displayShowDetails();
       break;
     case '/search.html':
-      console.log('Search');
+      search();
       break;
   }
 
